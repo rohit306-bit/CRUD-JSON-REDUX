0691963bba478436e9a02542cc02b798e961bfcc
rohit306-bit <76672224+rohit306-bit@users.noreply.github.com>
2021-12-17 18:27:37 +0530

    CRUD-JSON-REDUX

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Switch,Route } from 'react-router-dom';
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route  exact path="/" component= {Home} />
+      </Switch>
+   
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+};
+
+const usersReducers = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+export default usersReducers;
